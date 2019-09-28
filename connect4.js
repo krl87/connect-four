4b3d24777b23b0b7c06d619ef6c7b393be9d98f4
Kayley <kayley.luftig@gmail.com>
2019-09-27 21:23:12 -0400

    :construction: add functionality to hover over columns

--- connect4.js
+++ connect4.js
@@ -5,6 +5,7 @@ class Connect4 {
     this.COLS = 7;
     this.selector = selector;
     this.createGrid();
+    this.setupEventListeners();
   }
 
   //use method to build up grid divs
@@ -18,10 +19,38 @@ class Connect4 {
       //create for loop for columns within the rows
       for (let col = 0; col < this.COLS; col++) {
         const $col = $('<div>')
-          .addClass('col empty'); //give class empty for styling purposes
+          .addClass('col empty') //give class empty for styling purposes
+          .attr('data-col', col)
+          .attr('data-row', row);
         $row.append($col);
       }
       $board.append($row);
     }
+  }
+
+  setupEventListeners() {
+    const $board = $(this.selector);
+
+    function findLastEmptyCell(col) {
+      const cells = $(`.col[data-col='${col}']`);
+      for (let i = cells.length - 1; i >= 0; i--) {
+        const $cell = $(cells[i]);
+        if ($cell.hasClass('empty')) {
+          return $cell;
+        }
+      }
+      return null;
+    }
+
+    $board.on('mouseenter', '.col.empty', function() {
+      const col = $(this).data('col');
+      const $lastEmptyCell = findLastEmptyCell(col);
+      $lastEmptyCell.addClass(`next-red`);
+      console.log(col);
+    })
+
+    $board.on('mouseleave', '.col', function() {
+      $('.col').removeClass(`next-red`);
+    })
   }
 }
